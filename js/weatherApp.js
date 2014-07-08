062c216d702b84aa21e1a7d22e29361dea1c0f40
Pathik Gandhi <gr8pathik@gmail.com>
2014-07-08 10:18:27 +0530

    Final Upload

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -4,24 +4,32 @@ var app = angular.module('weatherApp', []);
 
 app.controller("weatherCtrl", ['$scope', 'weatherService', function ($scope,weather) {    
     $scope.metric = 'C';
-    $scope.metricText = '° '+$scope.metric;
-    
-    $scope.searchButtonText="Search";
-    $scope.cityName = 'Ahmedabad, India';
+    $scope.loading = false;
+    $scope.cityName = '';
     $scope.cityTitle = 'City Name';
 	$scope.cities = {};
 	$scope.forecast = {};
     $scope.loadForecast=function(e){
-    	console.log($scope.cityName);
-        weather.forecast($scope.cityName).then(function(data){
-        	console.log(data);
-        	$scope.currentForecast = data.data.city.id;
-            console.log(data.data.city);
-        	$scope.cities[$scope.currentForecast] = data.data.city;
-            $scope.forecast[$scope.currentForecast] = data.data;
-            $scope.cityTitle = $scope.forecast[$scope.currentForecast].city.name + ', ' + $scope.forecast[$scope.currentForecast].city.country;
-        console.log($scope.cities);
-        console.log($scope.forecast);
+    	$scope.getForecast($scope.cityName, 'q');
+    }
+
+    $scope.getForecast = function(value, type){
+        $scope.loading = true;
+        weather.forecast(value, type).then(function(data){
+            console.log(data);
+            if(data.data.cod == 200){
+                $scope.currentForecast = data.data.city.id;
+                console.log(data.data.city);
+                $scope.cities[$scope.currentForecast] = data.data.city;
+                $scope.forecast[$scope.currentForecast] = data.data;
+                $scope.cityTitle = $scope.forecast[$scope.currentForecast].city.name + ', ' + $scope.forecast[$scope.currentForecast].city.country;
+                console.log($scope.cities);
+                console.log($scope.forecast);
+            }else{
+                console.log("Not found");
+            }
+            $scope.cityName = '';
+            $scope.loading = false;
         });
     }
 
@@ -29,12 +37,44 @@ app.controller("weatherCtrl", ['$scope', 'weatherService', function ($scope,weat
         if(typeof temprature == 'undefined') temprature = 0;
         return Math.round(temprature - 273.15); //Convert Kelvin to Celsius
     }
+
+    $scope.getCurrentLocation = function(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition($scope.getCurrentLocationForecast);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    $scope.getCurrentLocationForecast = function(position){
+        $scope.getForecast({lat: position.coords.latitude.toFixed(2), lon: position.coords.longitude.toFixed(2)}, 'latlong');
+        
+    }
+
+    $scope.isEmpty = function (obj) {
+       return angular.equals({},obj); 
+    };
 }]);
 
 app.factory('weatherService',['$http',function($http){
     return {
-        forecast : function(locationName){
-            return $http.jsonp('http://api.openweathermap.org/data/2.5/forecast/daily?q='+locationName+'&cnt=14&APPID=xxxxx&callback=JSON_CALLBACK');
+        forecast : function(locationName, type){            
+            if(typeof type == 'undefined') type = 'q';
+            var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?';
+            if(type == 'latlong'){
+                url += 'lat='+locationName.lat+'&lon='+locationName.lon;
+            }else{
+                url += type+'='+locationName;
+            }
+            url += '&cnt=14&APPID=5667aae51435fa9043509ac68ae0ea8a&callback=JSON_CALLBACK';
+            return $http.jsonp(url);
         }
     }
 }]);
+
+app.filter('capitalize', function() {
+    return function(input, scope) {
+        if (input!=null)
+            return input.substring(0,1).toUpperCase()+input.substring(1);
+    }
+});
